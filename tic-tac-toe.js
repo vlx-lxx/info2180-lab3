9a7b4b92b5a7112cbf5d0a96ea336f7dc9e51a56
vlxcoding <vlxcoding@gmail.com>
2023-10-22 21:07:45 -0500

    Added an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,11 +7,41 @@ window.onload = function () {
 
   // Get all the div elements using a query selector
   const divs = board.querySelectorAll("div");
+  let plays = [null, null, null, null, null, null, null, null, null];
+  let clicke = true;
+  let play = "X";
 
   // Loop through the divs and set the "class" attribute to "square" for each of them
   divs.forEach((elem, index) => {
     elem.setAttribute("class", "square");
     console.log(elem, index);
+
+    elem.addEventListener("click", function () {
+      if (clicke) {
+        clicke = false;
+      } else {
+        clicke = true;
+      }
+
+      plays[index] = play;
+      elem.innerHTML = play;
+      elem.classList.add(play);
+
+      if (play == "O") {
+        play = "X";
+        plays[index] = play;
+        elem.innerHTML = play;
+        elem.classList.add(play);
+      } else {
+        play = "O";
+        plays[index] = play;
+        elem.innerHTML = play;
+        elem.classList.add(play);
+      }
+
+      console.log(clicke);
+      console.log(plays);
+    });
   });
 
   console.log(divs);
